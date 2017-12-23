95abe3bfd65830b7de804dcef3942fe31deb1b1e
Ali Asgar <aliasgar@outlook.com>
2017-12-23 18:03:37 +1100

    Update the logging configuration

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -19,6 +19,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
+    Logger.info("Rendering the index page")
     Ok(views.html.index())
   }
 }

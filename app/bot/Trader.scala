461fc6fc5edf4ea4a2ff5e9c6431266216ed6bef
Ali Asgar <aliasgar@outlook.com>
2017-12-28 21:05:09 +1100

    Implement the trade function

--- app/bot/Trader.scala
+++ app/bot/Trader.scala
@@ -12,21 +12,42 @@ class Trader @Inject()(command: ChartDataCommand) {
     val response: List[ChartResponse] = command.execute()
     Logger.info("response from poloniex {}", response)
 
+    val lengthOfMA = 10
     val prices: List[BigDecimal] = List(response.head.weightedAverage)
-    val tradePlaced = false
+    List("a", "b", "c").foldLeft(0)((l, r) => l + r.length)
     response.tail
-      .foreach(dataPoint => {
+      .foldLeft(false, "", prices)((prev, dataPoint) => {
+        val (prevTradePlaced, prevTypeOfTrade, prices) = prev
         val currentMovingAverage = prices.sum / prices.size
         val previousPrice = prices.last
         val lastPairPrice = dataPoint.weightedAverage
-        if (!tradePlaced) {
-          if ((lastPairPrice > currentMovingAverage) && (lastPairPrice < previousPrice)) {
-
-          } else if ((lastPairPrice < currentMovingAverage) && (lastPairPrice > previousPrice)) {
-
-          }
-        }
+        val (tradePlaced, typeOfTrade) = trade(prevTradePlaced, prevTypeOfTrade, currentMovingAverage, previousPrice, lastPairPrice)
+        val updatedPrices = (prices :+ lastPairPrice).takeRight(lengthOfMA)
+        Logger.info("{} Period: {}s {}: {} Moving Average: {}", dataPoint.date, "14400", "USDT_BTC", lastPairPrice, currentMovingAverage)
+        (tradePlaced, typeOfTrade, updatedPrices)
       })
 
+  }
+
+  private def trade(tradePlaced: Boolean, typeOfTrade: String, currentMovingAverage: BigDecimal, previousPrice: BigDecimal, lastPairPrice: BigDecimal): (Boolean, String) = {
+    if (!tradePlaced) {
+      if ((lastPairPrice > currentMovingAverage) && (lastPairPrice < previousPrice)) {
+        Logger.info("SELL ORDER")
+        (true, "short")
+      } else if ((lastPairPrice < currentMovingAverage) && (lastPairPrice > previousPrice)) {
+        Logger.info("BUY ORDER")
+        (true, "long")
+      } else {
+        (tradePlaced, typeOfTrade)
+      }
+    } else if (typeOfTrade == "short" && lastPairPrice < currentMovingAverage) {
+      Logger.info("EXIT TRADE")
+      (false, "")
+    } else if (typeOfTrade == "long" && lastPairPrice > currentMovingAverage) {
+      Logger.info("EXIT TRADE")
+      (false, "")
+    } else {
+      (tradePlaced, typeOfTrade)
+    }
   }
 }
--- app/bot/commands/ChartDataCommand.scala
+++ app/bot/commands/ChartDataCommand.scala
@@ -10,21 +10,21 @@ import play.api.libs.ws.{WSClient, WSResponse}
 import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
 
-class ChartDataCommand @Inject()(ws: WSClient)(implicit ec: ExecutionContext) extends HystrixCommand[Seq[ChartResponse]](HystrixCommandGroupKey.Factory.asKey("poloniex")) {
+class ChartDataCommand @Inject()(ws: WSClient)(implicit ec: ExecutionContext) extends HystrixCommand[List[ChartResponse]](HystrixCommandGroupKey.Factory.asKey("poloniex")) {
 
-  def run(): Seq[ChartResponse] = {
+  def run(): List[ChartResponse] = {
     val response: Future[WSResponse] = ws.url("https://poloniex.com/public")
       .addQueryStringParameters("command" -> "returnChartData")
       .addQueryStringParameters("currencyPair" -> "USDT_BTC")
-      .addQueryStringParameters("start" -> dateTimeToString(new DateTime().minusDays(1)))
+      .addQueryStringParameters("start" -> dateTimeToString(new DateTime().minusDays(30)))
       .addQueryStringParameters("end" -> dateTimeToString(new DateTime().plusDays(1)))
-      .addQueryStringParameters("period" -> "14400")
+      .addQueryStringParameters("period" -> 4.hours.toSeconds.toString)
       .addHttpHeaders("user-agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36")
       .addHttpHeaders("Cookie" -> "__cfduid=d8ad9b0167dc8b36d5528867a5c30bfb21509703260; _ga=GA1.2.817619400.1509703267; _gid=GA1.2.408801550.1514101253; POLOSESSID=tj6f45cclc1j405mpg0nl7msu7; cf_clearance=e88e296e2214480455696a547f7be5401fad0c99-1514102613-1800")
       .withRequestTimeout(10.seconds)
       .get()
     val future = response.map {
-      r => r.json.as[Seq[ChartResponse]]
+      r => r.json.as[List[ChartResponse]]
     }
     Await.result(future, 10.seconds)
   }
